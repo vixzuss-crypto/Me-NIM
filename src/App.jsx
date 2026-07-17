d399706f91e8bc7350f09ca767a0d5c694de51c7
vixzuss-crypto <vixzuss@gmail.com>
2026-07-17 07:53:09 +0700

    testing commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 flex flex-col items-center justify-center text-white">
+      <h1 className="text-4xl font-black text-red-500 tracking-wider mb-2">
+        ME-ANIM 🍿
+      </h1>
+      <p className="text-slate-400 font-medium text-sm">
+        Tailwind CSS + React di Termux udah sukses jalan!
+      </p>
+    </div>
+  );
+}
+
+export default App;
